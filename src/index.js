81cb5a29bed29807e4a632513c85ba292eb2a0e4
GHAITHBT <you@example.com>
2022-08-11 14:26:31 +0100

    last

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import Login1 from './Login';
 import Redir from './redirect'
 import EMPINTER from './Employé'
+import {Article} from './pages/Article';
+import {Caisse} from './pages/caisse';
+import {Clients} from './pages/Clients'
+import {Employee} from './pages/Liste_employé'
+import {Chart} from './pages/Charts'
+import {BL} from'./pages/Bon_Livraison'
+import {CM} from './pages/commande'
+import { Facture } from './pages/Facture';
+import PrintFact from './pages/PRINTFACT';
+import PrintBC from './pages/PrintBC'
 ReactDOM.render(
   <React.StrictMode>
-<Redir/>
+<Facture/>
  </React.StrictMode>,
   document.getElementById('root')
 );
